51c2f39aeb9f61e16a067dfd4dd94919b541ab59
Ichabod-Carne <18829025242@163.com>
2022-07-04 20:03:14 +0800

    目录结构变更

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var userCtrl = require("../controller/user");
-
-/* 注册用户 */
-router.post("/regiup", userCtrl.signup);
-
-module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,29 +1,23 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import indexRouter from "./routes/index";
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
-var app = express();
-
-require(process.cwd() + "/common/sql.js");
-require(process.cwd() + "/common/utils.js");
+const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -41,4 +35,4 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-module.exports = app;
+export default app;
